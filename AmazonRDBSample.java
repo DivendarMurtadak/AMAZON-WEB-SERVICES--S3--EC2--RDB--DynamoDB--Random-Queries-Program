fef60f24e7a9890e4e6b5b9257377bc60a6d7e04
divendar murtadak <divendar.murtadak@gmail.com>
2014-08-19 11:45:41 -0500

    Update AmazonRDBSample.java

--- AmazonRDBSample.java
+++ AmazonRDBSample.java
@@ -1,13 +1,10 @@
-
 /* References:
-http://aws.amazon.com/apache2.0
-http://www.java-tips.org/other-api-tips/jdbc/import-data-from-txt-or-csv-files-into-mysql-database-t-3.html
-http://stackoverflow.com/questions/20279273/upload-csv-file-containing-million-values-to-a-particular-column-in-mysql
-http://stackoverflow.com/questions/3635166/how-to-import-csv-file-to-mysql-table
-http://viralpatel.net/blogs/java-load-csv-file-to-database/
-*/
-
-
+ http://aws.amazon.com/apache2.0
+ http://www.java-tips.org/other-api-tips/jdbc/import-data-from-txt-or-csv-files-into-mysql-database-t-3.html
+ http://stackoverflow.com/questions/20279273/upload-csv-file-containing-million-values-to-a-particular-column-in-mysql
+ http://stackoverflow.com/questions/3635166/how-to-import-csv-file-to-mysql-table
+ http://viralpatel.net/blogs/java-load-csv-file-to-database/
+ */
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -16,14 +13,9 @@ import java.util.Date;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
+public class AmazonRDBSample {
 
-public class AmazonRDBSample{
-	
-
-	 
-	   
-
-	    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 	        
 System.out.println("Table Creation Example!");
  Connection con = null;
@@ -41,12 +33,12 @@ System.out.println("Table Creation Example!");
           
      con = DriverManager.getConnection("jdbc:mysql://Divendar.cury5aj8qlt4.us-west-2.rds.amazonaws.com:3306/innodb","cloud3","akdv1234");
      System.out.println("Connected to the database....");
-     con = DriverManager.getConnection("jdbc:mysql://Divendar.cury5aj8qlt4.us-west-2.rds.amazonaws.com:3306/innodb","Divendar","akdv1234");
-  con = DriverManager.getConnection("jdbc:mysql://localhost:3306/innodb","Divendar","akdv1234");
+     //con = DriverManager.getConnection("jdbc:mysql://Divendar.cury5aj8qlt4.us-west-2.rds.amazonaws.com:3306/innodb","Divendar","akdv1234");
+  //con = DriverManager.getConnection("jdbc:mysql://localhost:3306/innodb","Divendar","akdv1234");
  try{
 	 jdbc:mysql:localhost/jdbcdb////Divendar.cury5aj8qlt4.us-west-2.rds.amazonaws.com:3306
   Statement st = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
- // String filename= "weather-100K.csv";
+  String filename= "weather-100K.csv";
   BufferedReader br = new BufferedReader(new FileReader(filename));
 	int iKey=1;
 	Connection connection = new getConnection();
@@ -58,8 +50,6 @@ System.out.println("Table Creation Example!");
 		   String[] sTemp = sEachLine.split(Pattern.quote(","));
 		   String query = " INSERT INTO `innodb`.`weather_100k` (`Id`, `STATION`, `STATION_NAME`, `DATE`, `TMAX`, `TMIN`, `TOBS`, `WDMV`, `AWND`, `WDF2`, `WDF5`, `WDFG`, `WSF2`, `WSF5`, `WSFG`, `PGTM`, `FMTM`) VALUES ("
 		   		+iKey+", '"+sTemp[0]+"' , '"+sTemp[1]+"' , '"+sTemp[2]+"' , '"+sTemp[3]+"' , '"+sTemp[4]+"' , '"+sTemp[5]+"' , '"+sTemp[6]+"' , '"+sTemp[7]+"' , '"+sTemp[8]+"' , '"+sTemp[9]+"' , '"+sTemp[10]+"' , '"+sTemp[11]+"' , '"+sTemp[12]+"' , '"+sTemp[13]+"' , '"+sTemp[14]+"' , '"+sTemp[15]+"' );";
-				   //"insert into weather (name, city) values('"
-		            //+ employee.getName() + "','" + employee.getCity + "')";
 		    statement.addBatch(query);
 		    if ((iKey) % 1000 == 0) {
 		    	System.out.println("in executing batch");
@@ -74,7 +64,6 @@ System.out.println("Table Creation Example!");
 	con.close();
 	
   String  sQuery=" INSERT INTO `innodb`.`weather_100k` (`Id`, `STATION`, `STATION_NAME`, `DATE`, `TMAX`, `TMIN`, `TOBS`, `WDMV`, `AWND`, `WDF2`, `WDF5`, `WDFG`, `WSF2`, `WSF5`, `WSFG`, `PGTM`, `FMTM`) VALUES ('1', 'test', 'test', '00000000', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test');"; 
-		  //"LOAD DATA INFILE '" +filename+"' INTO TABLE weather FIELDS TERMINATED BY '"+","+"' (STATION, STATION_NAME, DATE, TMAX, TMIN, TOBS, WDMV, AWND, WDF2, WDF5, WDFG, WSF2, WSF5, WSFG, PGTM, FMTM) ; ";
   st.executeUpdate(sQuery);
 
 	
